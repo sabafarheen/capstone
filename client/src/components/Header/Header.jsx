1f298a8ffb6892d1ff41886f05727e7181dc7575
Saba Farheen <sabafarheen2003@gmail.com>
2022-01-23 21:12:40 -0800

    Linked home and resource page to the header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import Logo from "../../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
@@ -22,9 +23,12 @@ const Header = () => {
     <header className="header">
       <img className="header__logo" src={Logo} alt="Gi4Kids logo" />
       <ul className="header__nav">
-        <li className="header__nav-link">Home</li>
-        <li className="header__nav-link">Resource</li>
-
+        <Link to="/">
+          <li className="header__nav-link">Home</li>
+        </Link>
+        <Link to="/grade">
+          <li className="header__nav-link">Resource</li>
+        </Link>
         {/* {token && ( */}
         <li className="header__nav-link-color" onClick={logOutHandler}>
           Logout
